01d855843c5a03e0ea99ca106ff9eeb244207cc1
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2023-02-15 14:45:59 -0600

    added event listener
    included a form and an event listener function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from "./logobw.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid"
         alt="logo" />
-        Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer
+         className="App-footer"><small>Coded by Tiffany Walker</small>
+      </footer>
+    </div>
     </div>
   );
 }
