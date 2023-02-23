bbb53672bd8c165f8120474f85dabf80975dad6d
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2023-02-22 22:16:37 -0600

    added photos api
    added keyword and api linking to photos and original photo plus css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,39 @@
 import React, {useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos"; 
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults]= useState(null);
     let [loaded, setLoaded]=useState(false);
+    let [photos, setPhotos]=useState(null);
 
-    function handleResponse (response){
+    function handleDictionaryResponse (response){
         setResults(response.data[0]);
     }
+
+    function handlePexelsResponse(response){
+      setPhotos(response.data.photos);
+      }
 
     function search (){
       // doucmentation: https://dictionaryapi.dev/e
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexelsApiKey=
+        "Pa7dTbZpHB4WaUtkoupqkZallAG0b72JCVQTHCAHvQORWgDAuP7YGIch";
+        let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers= { Authorization: `Bearer ${pexelsApiKey}` };
+         axios.get(pexelsApiUrl, { headers:  headers }).then(handlePexelsResponse);
 }   
 
-function handleSubmit (event){
+ function handleSubmit (event){
     event.preventDefault();
     search();
-}
+}  
 
 function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -49,6 +61,7 @@ if (loaded){
             </div>
             </section>
         <Results results={results} />
+        <Photos photos={photos} />
         </div>
         );
 } else {
